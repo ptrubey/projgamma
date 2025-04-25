cad1fd6286610f3ef0b44692ebad78a71d5c9e8a
Peter Trubey <peter@trubey.org>
2025-04-25 12:46:49 -0600

    genpareto

---
+++ genpareto.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.optimize import minimize
+from itertools import repeat
+
+def gpd_fit(data, threshold):
+    " fits the GPD parameters for a given threshold "
+    diff = data - threshold
+    excess = diff[diff > 0]
+    xbar = excess.mean()
+    s2   = excess.var()
+    # xi0  = -0.5 * (((xbar * xbar) / s2) - 1.)
+    xi0 = np.linspace(-1,1,10)
+    sc0 = 0.5 * xbar * (((xbar * xbar) / s2) + 1)
+    theta_mat = np.array(list(zip(xi0, repeat(sc0))))
+
+    # negative log-likelihood under gen pareto model
+    def gpd_neg_log_lik(theta):
+        sc, xi = theta
+        cond1 = sc <= 0.
+        cond2 = (xi <= 0.) and (excess.max() > (-sc / xi))
+        if (cond1 or cond2):
+            return 1e9
+        else:
+            y = np.log(1 + (xi * excess) / sc) / xi
+            return len(excess) * np.log(sc) + (1 + xi) * y.sum()
+    
+    # wrapper for minimize function
+    def gpd_nll_wrapper(theta):
+        fit = minimize(gpd_neg_log_lik, theta, method = 'L-BFGS-B')
+        return np.hstack((fit.x, gpd_neg_log_lik(fit.x)))
+
+    # try model fitting at a range of values for xi.
+    res = np.array(list(map(gpd_nll_wrapper, theta_mat)))
+    # Verify the model actually successfully fit something
+    try: 
+        assert not np.all(np.abs(res.T[2] - res.T[2].max()) < 1e-9)
+    except AssertionError:
+        print('Model Fitting Unsuccessful')
+    # return those fitted parameters
+    return res[res.T[2].argmin(),:2]
+
+
+# EOF
